473cac16be142c39d25009d053f86f9f1299bb3c
Melih Elibol <elibol@gmail.com>
2018-04-06 15:19:18 -0700

    object manager benchmark tool

--- src/ray/object_manager/test/send.sh
+++ src/ray/object_manager/test/send.sh
@@ -1,10 +1,12 @@
-CORE_DIR="/home/elibol/dev/ray/python/ray/core"
+#!/usr/bin/env bash
+
+CORE_DIR="${CORE_DIR:-/home/ubuntu/ray/python/ray/core}"
 
 node_ip_address="127.0.0.1"
 redis_address="127.0.0.1"
 redis_port="6379"
 store_executable="$CORE_DIR/src/plasma/plasma_store"
-om_exec="$CORE_DIR/src/ray/object_manager/om_multinode"
+om_exec="$CORE_DIR/src/ray/object_manager/object_manager_benchmark_tool"
 
 mode="send"
 object_size=1000000000
